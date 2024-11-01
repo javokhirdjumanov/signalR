3932e2f3a498376f39e3196f5f95c1d49fa99765
Javohir <jumanovjavohir49@gmail.com>
2024-11-01 17:19:16 +0500

    add

---
+++ src/Core/Extensions/ServiceExtension.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Middlewares;
+using Microsoft.AspNetCore.Builder;
+
+namespace Core.Extensions;
+public static class ServiceExtension
+{
+    public static WebApplication ConfigureDefaults(this WebApplication app, string? baseUrl = null)
+    {
+        app.UseCors();
+
+        app.UseMiddleware<GlobalExceptionHandlerMiddleware>();
+
+        app.UseHealthChecks("/healthy");
+        app.UseAuthorization();
+
+        return app;
+    }
+}
---
+++ src/Domain/Middlewares/GlobalExceptionHandlerMiddleware.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Exceptions;
+using Microsoft.AspNetCore.Http;
+
+namespace Domain.Middlewares;
+public class GlobalExceptionHandlerMiddleware : IMiddleware
+{
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (Exception ex)
+        {
+            if (ex is ApiException apiException)
+            {
+                context.Response.StatusCode = apiException.StatusCode;
+                //await context.Response.WriteAsJsonAsync<WrapperGeneric<object>>(WrapperGeneric<object>.ResultFromException(ex, (HttpStatusCode)apiException.StatusCode));
+            }
+            else
+            {
+                context.Response.StatusCode = 500;
+                //await context.Response.WriteAsJsonAsync<WrapperGeneric<object>>(WrapperGeneric<object>.ResultFromException(ex));
+            }
+        }
+    }
+}
